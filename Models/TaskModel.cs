09b4f04bca3401a129ccb5293ba01729404027c8
Moran <alexmoranduque@gmail.com>
2023-04-27 19:14:23 -0600

    Added scaffold files

---
+++ Models/TaskModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUD.Models;
+
+public class TaskModel
+{
+    public string? TaskName { get; set; }
+    
+    public bool IsPrivate { get; set; }
+
+    private DateTime DueDate { get; set; }
+    
+}
